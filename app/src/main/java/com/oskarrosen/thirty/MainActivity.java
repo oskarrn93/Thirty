94286136d1857d6645478dcd644f72f1562078f2
Oskar Rosén <oskar@oskarrosen.com>
2019-06-11 22:11:38 +0200

    Added game activity

--- app/src/main/java/com/oskarrosen/thirty/MainActivity.java
+++ app/src/main/java/com/oskarrosen/thirty/MainActivity.java
@@ -2,13 +2,40 @@ package com.oskarrosen.thirty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private Button mButtonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonStart = findViewById(R.id.buttonStart);
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "mButtonStart click listener");
+                showThirtyActivity();
+            }
+        });
+
+
+    }
+
+    private void showThirtyActivity() {
+        Log.d(TAG,"showThirtyctivity");
+
+        Intent intent = new Intent(getBaseContext(), ThirtyActivity.class);
+        //intent.putExtra("EXTRA_ANSWER_IS_TRUE", answerIsTrue);
+        startActivityForResult(intent, 0);
     }
 }
